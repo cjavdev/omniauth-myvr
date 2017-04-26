fb5cb69cf098831c76ab2c25c9cbf844a4b6802e
CJ Avilla <cjavilla@gmail.com>
2017-04-26 13:31:37 -0700

    add credentials block

--- lib/omniauth/strategies/myvr.rb
+++ lib/omniauth/strategies/myvr.rb
@@ -1,26 +1,13 @@
-require 'multi_json'
-require 'jwt'
-require 'omniauth/strategies/oauth2'
-require 'uri'
+require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
     class Myvr < OmniAuth::Strategies::OAuth2
-      if ENV['MYVR_DEV']
-        BASE_SCOPE_URL = "http://local.api.myvr.com:8000/auth/"
-        option :client_options, {
-          :site          => 'http://local.myvr.com:8000/',
-          :authorize_url => 'http://local.myvr.com:8000/connect/oauth/auth/',
-          :token_url     => 'http://api.local.myvr.com:8000/oauth/token/'
-        }
-      else
-        BASE_SCOPE_URL = "https://api.myvr.com/auth/"
-        option :client_options, {
-          :site          => 'https://myvr.com/',
-          :authorize_url => 'https://myvr.com/connect/oauth/auth/',
-          :token_url     => 'https://api.myvr.com/oauth/token/'
-        }
-      end
+      option :client_options, {
+        :site          => 'https://myvr.com/',
+        :authorize_url => 'https://myvr.com/connect/oauth/auth/',
+        :token_url     => 'https://api.myvr.com/oauth/token/'
+      }
 
       BASE_SCOPES = %w[property_read]
       DEFAULT_SCOPE = 'property_read'
@@ -28,12 +15,12 @@ module OmniAuth
       option :name, 'myvr'
       option :authorize_params, {grant_type: 'authorization_code'}
 
+      credentials do
+        access_token.params
+      end
+
       def build_access_token
-        token_params = {
-          :client_secret => client.secret,
-          :grant_type => 'authorization_code',
-          :parse => true
-        }
+        token_params = {}
         verifier = request.params['code']
         client.auth_code.get_token(verifier, token_params)
       end
